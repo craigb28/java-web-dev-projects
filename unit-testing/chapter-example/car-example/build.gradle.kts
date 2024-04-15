8ae64d1e051144f21d5f48fa89458e778c127e1c
Craig Brummer <craigbrummer28@gmail.com>
2024-04-15 17:28:15 -0500

    examples complete

--- unit-testing/chapter-example/car-example/build.gradle.kts
+++ unit-testing/chapter-example/car-example/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
+dependencies{
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
