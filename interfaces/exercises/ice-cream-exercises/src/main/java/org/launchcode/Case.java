9504f0bdcb11e2b8797e42b8fbb229b6e0deb197
Craig Brummer <craigbrummer28@gmail.com>
2024-04-22 10:09:26 -0500

    exercises complete

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Case.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Case.java
@@ -7,26 +7,36 @@ public class Case {
     private ArrayList<Flavor> flavors = new ArrayList<>();
     private ArrayList<Cone> cones = new ArrayList<>();
 
-    public Case(){
-        Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList( "gluten")));
-        Cone cone2 = new Cone("Sugar", 0.75, new ArrayList<>(Arrays.asList( "gluten")));
-        Cone cone3 = new Cone("Wafer", 0.50, new ArrayList<>(Arrays.asList( "gluten")));
-        Cone cone4 = new Cone("Bowl", 0.05, new ArrayList<>(Arrays.asList( "none")));
+    private ArrayList<Topping> toppings = new ArrayList<>();
+
+    public Case() {
+        Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList("gluten")));
+        Cone cone2 = new Cone("Sugar", 0.75, new ArrayList<>(Arrays.asList("gluten")));
+        Cone cone3 = new Cone("Wafer", 0.50, new ArrayList<>(Arrays.asList("gluten")));
+        Cone cone4 = new Cone("Bowl", 0.05, new ArrayList<>(Arrays.asList("none")));
         cones.add(cone1);
         cones.add(cone2);
         cones.add(cone3);
         cones.add(cone4);
 
-        Flavor flavor1 = new Flavor("Vanilla", 0.75, new ArrayList<>(Arrays.asList( "dairy")));
-        Flavor flavor2 = new Flavor("Chocolate", 0.75, new ArrayList<>(Arrays.asList( "dairy")));
-        Flavor flavor3 = new Flavor("Red Velvet", 0.85, new ArrayList<>(Arrays.asList( "dairy", "red #5")));
-        Flavor flavor4 = new Flavor("Rocky Road", 0.85, new ArrayList<>(Arrays.asList( "dairy", "nuts", "gelatin")));
-        Flavor flavor5 = new Flavor("Strawberry Sorbet", 0.50, new ArrayList<>(Arrays.asList( "strawberry")));
+        Flavor flavor1 = new Flavor("Vanilla", 0.75, new ArrayList<>(Arrays.asList("dairy")));
+        Flavor flavor2 = new Flavor("Chocolate", 0.75, new ArrayList<>(Arrays.asList("dairy")));
+        Flavor flavor3 = new Flavor("Red Velvet", 0.85, new ArrayList<>(Arrays.asList("dairy", "red #5")));
+        Flavor flavor4 = new Flavor("Rocky Road", 0.85, new ArrayList<>(Arrays.asList("dairy", "nuts", "gelatin")));
+        Flavor flavor5 = new Flavor("Strawberry Sorbet", 0.50, new ArrayList<>(Arrays.asList("strawberry")));
         flavors.add(flavor1);
         flavors.add(flavor2);
         flavors.add(flavor3);
         flavors.add(flavor4);
         flavors.add(flavor5);
+
+        Topping topping1 = new Topping("fudge", 1.25, new ArrayList<>(Arrays.asList("chocolate")));
+        Topping topping2 = new Topping("caramel", 0.75, new ArrayList<>(Arrays.asList("sugar")));
+        Topping topping3 = new Topping("sprinkles", 0.50, new ArrayList<>(Arrays.asList("sugar")));
+        toppings.add(topping1);
+        toppings.add(topping2);
+        toppings.add(topping3);
+
     }
 
     public ArrayList<Flavor> getFlavors() {
@@ -43,5 +53,13 @@ public class Case {
 
     public void setCones(ArrayList<Cone> cones) {
         this.cones = cones;
+    }
+
+    public ArrayList<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(ArrayList<Topping> toppings) {
+        this.toppings = toppings;
     }
 }
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,17 +1,38 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+// TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
+//        Comparator<Flavor> comparator = new FlavorComparator();
+//        flavors.sort(comparator);
+
+        flavors.sort(new FlavorComparator());
+
+        System.out.println(flavors);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
+//Comparator<Cone> coneComparator = new ConeComparator();
+//cones.sort(coneComparator);
+//
+//        System.out.println(cones);
+
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        Comparator<Topping> toppingComparator = new ToppingComparator();
+        toppings.sort(toppingComparator);
+
+        System.out.println(toppings);
+//
+//        System.out.println(cones);
+
     }
 }
